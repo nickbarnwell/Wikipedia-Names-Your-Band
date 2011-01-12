0582c026a084e629695b3cb0a181e30443f4d0d4
Nick Barnwell <nickbarnwell@boltoncomputing.com>
2011-01-12 12:16:30 +0100

    Moved to HAML for templating, borrowed from Kittygram's style, changed how fetching works
    Updated gemfile to include haml
    
    removed extraneous character in main

--- wikipedanyb.rb
+++ wikipedanyb.rb
@@ -3,37 +3,53 @@ require 'sinatra'
 require 'mechanize'
 require 'rubygems'
 require 'erb'
+require 'haml'
 
-before '/' do
-	# Initialize Mechanize
+
+def fetch_results(num)
 	@agent ||= Mechanize.new
 	@agent.user_agent_alias = 'Mac Safari'
-	@page = @agent.get('http://en.wikipedia.org/wiki/Special:Random')
+	results = []
+	(1..num).each do
+		page = @agent.get('http://en.wikipedia.org/wiki/Special:Random')
+		bandname =  page.title.split('-')[0]
+		page = @agent.get('http://www.quotationspage.com/random.php3')
+
+		doc = page.parser.xpath('//dt[@class="quote"]/a')
+
+		link = doc.last
+		t = link.content.split(' ')
+		albumtitle = t.slice(t.length-5,t.length).join(' ')
+
+		page = @agent.get('http://www.flickr.com/explore/interesting/7days/')
+		
+		doc = page.parser.xpath('//span[@class="photo_container pc_m"]/a')
+
+		photo = doc[2]
+		url = 'http://flickr.com'+photo.attribute('href').value
+		page = @agent.get(url)
+		
+		doc = page.parser.xpath('//img[@alt="photo"]')
+		photo = doc[0].attribute('src').value
+		
+		results << {
+			'url' => url,
+			'photo' => photo,
+			'bandname' => bandname,
+			'albumtitle' => albumtitle
+			}
+	end
+	results
 end
 
 get '/' do
-	@bname =  @page.title.split('-')[0]
-	@page = @agent.get('http://www.quotationspage.com/random.php3')
-
-	doc = @page.parser.xpath('//dt[@class="quote"]/a')
-
-	link = doc.last
-	t = link.content.split(' ')
-	@title = t.slice(t.length-5,t.length).join(' ')
-
-	@page = @agent.get('http://www.flickr.com/explore/interesting/7days/')
-
-	doc = @page.parser.xpath('//span[@class="photo_container pc_m"]/a')
-
-	@photo = doc[2]
-	@photo.attribute('href').value = 'http://flickr.com'+@photo.attribute('href')
-	#puts @bname, @title
-	erb :index
+	@results = fetch_results(5)
+	haml(:index)
 end
 
 not_found do
 	status 404
-	'That wasn\'t found, sorry. Try checking out the readme to see if you were accessing an unsupported method'
+	'That page wasn\'t found, sorry.'
 end
 
 error do
